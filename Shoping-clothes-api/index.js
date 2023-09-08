22ef2fa5fcaef9276f1211f33f7c476d1fc65771
goku673 <zapatamorato@gmail.com>
2023-09-08 01:11:23 -0400

    initial comit

--- Shoping-clothes-api/index.js
+++ Shoping-clothes-api/index.js
@@ -22,7 +22,7 @@ servidor.use(bodyParser.json());
 
 
 servidor.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Origin', 'https://deploy-shoping-clothes.vercel.app');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
